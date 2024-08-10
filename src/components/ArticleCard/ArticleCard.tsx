520311a33a772dea49275541d68aa2c445bd25dc
Hassaan Bin Sajid <hassaan.bs25@gmail.com>
2024-08-10 12:38:01 +0500

    Updated Article Card Component

--- src/components/ArticleCard/ArticleCard.tsx
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -13,7 +13,22 @@ import {
 import placeHolderImg from "../../assets/placeholder-img.jpg";
 import { Article } from "../../types";
 
+const options: Intl.DateTimeFormatOptions = {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  timeZoneName: "short",
+};
+
 const ArticleCard = ({ article }: { article: Article }) => {
+  const formattedDate = new Date(article.publishDate).toLocaleDateString(
+    "en-US",
+    options
+  );
+
   return (
     <Card
       className="article"
@@ -38,10 +53,17 @@ const ArticleCard = ({ article }: { article: Article }) => {
             transitionTimingFunction="cubic-bezier(.4,0,.2,1)"
             _hover={{ transform: "scale(1.05)" }}
           />
+
+          <Text fontSize="md" mb={2} color={"#7C7D7D"}>
+            Published on {formattedDate}
+          </Text>
           <Box mb={4}>
-            <Text fontSize="md" mb={4}>
-              Source: {article.source}
-            </Text>
+            {article.source && (
+              <Text fontSize="md" mb={4}>
+                Source: {article.source}
+              </Text>
+            )}
+
             <Heading as="h2" fontSize="xl" mb={2}>
               {article.title}
             </Heading>
