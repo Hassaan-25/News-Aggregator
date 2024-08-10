fbd5bbbd88fa33a13c4b42586ec1ec603c9daf34
Hassaan Bin Sajid <hassaan.bs25@gmail.com>
2024-08-10 12:38:18 +0500

    updated FilterBar Component

--- src/components/FilterBar/FilterBar.tsx
+++ src/components/FilterBar/FilterBar.tsx
@@ -1,88 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { useFiltersContext } from "../../context/FiltersContext";
-import { Select, Box } from "@chakra-ui/react";
-import { SingleDatePicker } from "react-dates";
-import "react-dates/initialize";
-import "react-dates/lib/css/_datepicker.css";
-import moment from "moment";
-import "../../App.css";
+import { Box } from "@chakra-ui/react";
+import SourceDropdown from "../SourceDropdown";
+import SearchInput from "../SearchInput/SearchInput";
+import CategoryDropdown from "../CategoryDropdown";
+import DatePicker from "../DatePicker";
 
 const FilterBar = () => {
-  const {
-    filters,
-    actions: { updateFilters },
-  } = useFiltersContext();
-  const [category, setCategory] = useState(filters.category);
-  const [date, setDate] = useState(filters.date ? moment(filters.date) : null);
-  const [focused, setFocused] = useState(false);
-
-  useEffect(() => {
-    updateFilters({
-      ...filters,
-      category,
-      date: date ? date.format("YYYY-MM-DD") : "",
-    });
-  }, [category, date, updateFilters, filters]);
-
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      flexDirection={["row-reverse", "row"]}
-      gap={4}
-    >
-      <Box display="flex" flexDirection={"column"} gap={2}>
-        <label
-          htmlFor="category"
-          style={{
-            fontSize: "14px",
-            color: "black",
-          }}
-        >
-          Pick a Category:
-        </label>
-        <Select
-          placeholder="All Categories"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-          size={"lg"}
-          width={["150px", "300px"]}
-          background={"white"}
-          color={category === "" ? "#718096" : "initial"}
-        >
-          <option value="technology">Technology</option>
-          <option value="business">Business</option>
-          <option value="sports">Sports</option>
-        </Select>
-      </Box>
-      <Box
-        display="flex"
-        flexDirection={"column"}
-        gap={2}
-        width={["150px", "200px"]}
-        borderRadius={10}
-        zIndex={10}
-      >
-        <label
-          htmlFor="date_picker"
-          style={{
-            fontSize: "14px",
-            color: "black",
-          }}
-        >
-          Date:
-        </label>
-        <SingleDatePicker
-          date={date}
-          onDateChange={(date) => setDate(date)}
-          focused={focused}
-          onFocusChange={({ focused }) => setFocused(focused)}
-          id="date_picker"
-          numberOfMonths={1}
-          isOutsideRange={() => false}
-          displayFormat="YYYY-MM-DD"
-        />
-      </Box>
+    <Box display="flex" flexDirection={["column", "row"]} gap={8}>
+      <SearchInput />
+      <CategoryDropdown />
+      <SourceDropdown />
+      <DatePicker />
     </Box>
   );
 };
