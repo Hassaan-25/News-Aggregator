a07c0b70699e5d8866c807c8567e426ce2c01aa7
Hassaan Bin Sajid <hassaan.bs25@gmail.com>
2024-08-11 19:59:38 +0500

    Updated App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home";
-// import PreferencesPage from "./pages/PreferencesPage";
-import FiltersContext from "./context/FiltersContext";
-import { ChakraBaseProvider, theme as chakraTheme } from "@chakra-ui/react";
-import "react-dates/initialize";
 import Nav from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import FiltersContext from "./context/FiltersContext";
+import { ChakraBaseProvider, theme as chakraTheme } from "@chakra-ui/react";
 
 const App = () => {
   return (
     <FiltersContext>
-      <Nav />
       <ChakraBaseProvider theme={chakraTheme}>
+        <Nav />
         <Router>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            {/* <Route path="/preferences" element={<PreferencesPage />} /> */}
           </Routes>
         </Router>
+        <Footer />
       </ChakraBaseProvider>
-      <Footer />
     </FiltersContext>
   );
 };
