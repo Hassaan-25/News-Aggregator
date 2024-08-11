9878c995fee43e2bca1dc07c2ea158e67612bae2
Hassaan Bin Sajid <hassaan.bs25@gmail.com>
2024-08-11 20:00:57 +0500

    Created Constant Colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,15 @@
+
+export const Colors = {
+    black: "#000000",
+    white: "#ffffff",
+    gray: "#7C7D7D",
+    red: "#F44336",
+    blue: "#2196F3",
+    orange: "#FF9800",
+
+    textSecondary: "#7C7D7D",
+    placeHolderText: "#718096",
+    background: "#f5f5f5",
+    SearchBackground: "#fffff",
+    LinkColor: "blue.500"
+}
